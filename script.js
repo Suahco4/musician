15b739cdecc88936663971101cacdd98fc0fb65b
Suahco4 <Thesuah22@gmail.com>
2025-06-03 13:22:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function showStory() {
         Now, as I step into education, I aspire to nurture young minds while continuing my web development journey to bring creative ideas to life.
     `;
 }
+function toggleInfo() {
+    let infoText = document.getElementById("info-text");
+    if (infoText.innerHTML.includes("Click the button")) {
+        infoText.innerHTML = `
+            <ul>
+                <li><strong>Name:</strong> Emmanuel Suah</li>
+                <li><strong>Profession:</strong> Musician, Community Leader, Aspiring Educator</li>
+                <li><strong>Skills:</strong> Web Development, Music Production, Teaching Techniques</li>
+                <li><strong>Interests:</strong> Ministry Outreach, Music Promotion, Education</li>
+            </ul>
+        `;
+    } else {
+        infoText.innerHTML = "Click the button to view my details.";
+    }
+}
